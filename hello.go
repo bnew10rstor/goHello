f4cb6eca9c025bdabe83910650cd69538715eae8
Bradley Newton <bradley.newton@rstor.io>
2018-05-21 10:32:29 -0700

    added stringutil functionality

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import  (
+	 "fmt"
+	 "github.com/bnew10rstor/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("\n")
 }
